aa48562a3be8529c3de9c24207b84ffbfbcd1764
Jin <jin@MacMiNis-Mac-mini.local>
2014-02-11 10:04:36 +0700

    Initial Commit

---
+++ LocalCIXcode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LocalCIXcode
+//
+//  Created by Jin on 2/11/14.
+//  Copyright (c) 2014 Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LocalCIXcode/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LocalCIXcode
+//
+//  Created by Jin on 2/11/14.
+//  Copyright (c) 2014 Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
